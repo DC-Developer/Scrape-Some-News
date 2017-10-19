91ad7e4710a3ae1945ac6f8a35260c101d6cf488
dionCavanaugh <dioncavanaugh@hotmail.com>
2017-10-19 02:46:42 -0400

    added ajax delete functionality, handlebars to populate if target article is saved

--- public/assests/app.js
+++ public/assests/app.js
@@ -1,27 +1,20 @@
-// $(document).on("click", "#delete", function(){
-//     // console.log("you clicked the delete button");
+$(document).on("click", "#delete", function(){
+    // console.log("you clicked the delete button");
     
-//     var thisId = $(this).attr("data-id");
+    var thisId = $(this).attr("data-id");
     
-//     $.ajax({
-//         method: "POST",
-//         url: "api/articles/delete/" + thisId
-//         //creating a data object to hold the deleted buttons div, which is selected with their id
-//         // data: {
-//         //   title: $("#titleDiv").val(),
-//         // }
-//       })
+    $.ajax({
+        method: "POST",
+        url: "api/articles/delete/" + thisId
+        //creating a data object to hold the deleted buttons div, which is selected with their id
+        // data: {
+        //   title: $("#titleDiv").val(),
+        // }
+      })
         
-//         .done(function(data) {
-            
-//         //   console.log(data);
-//           console.log("you clicked the delete button");
-//           // $(this).empty();
-//         });
-    
-        
-      
-//     //   $("#titleinput").val("");
-//     //   $("#bodyinput").val("");
-    
-// });
+        .done(function(data) {
+          
+            location.reload();
+            console.log("you clicked the delete button");
+        });
+});
